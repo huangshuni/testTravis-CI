ed92ff174f021b77e52e31eef2565855e613a88a
huangshuni <15889638723@163.com>
2017-05-22 09:57:39 +0800

    init
    Signed-off-by: huangshuni <15889638723@163.com>

---
+++ testTravis/testTravis/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testTravis
+//
+//  Created by 黄淑妮 on 2017/5/22.
+//  Copyright © 2017年 黄淑妮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ testTravis/testTravis/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  testTravis
+//
+//  Created by 黄淑妮 on 2017/5/22.
+//  Copyright © 2017年 黄淑妮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
